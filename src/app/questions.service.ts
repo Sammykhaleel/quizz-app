b103f6f9ca95a06921d03b513f0ca8a7202ca376
JessBob <54863103+Jessbob@users.noreply.github.com>
2019-12-17 11:11:45 -0600

    Fix url location in http.get()

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -10,7 +10,7 @@ export class QuestionsService {
   constructor(private http: HttpClient) {}
 
   public getQuizzes() {
-    return this.http.get(`../assets/quiz-list.json`).pipe(
+    return this.http.get(`./assets/quiz-list.json`).pipe(
       map((results: any[]) => {
         return results.map(
           r => new Quiz(r.label, r.name, r.description, r.filename)
@@ -20,7 +20,7 @@ export class QuestionsService {
   }
 
   public getQuestions(fileName: string) {
-    return this.http.get(`../assets/${fileName}.json`).pipe(
+    return this.http.get(`./assets/${fileName}.json`).pipe(
       map((result: any[]) => {
         return result.map(r => new Question(r.label, r.choices));
       })
